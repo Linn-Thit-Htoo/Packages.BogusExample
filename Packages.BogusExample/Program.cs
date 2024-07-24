e2d74a556330cd208f87eeb4c2d13e0c2bce7130
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-24 20:13:28 +0630

    -

--- Packages.BogusExample/Program.cs
+++ Packages.BogusExample/Program.cs
@@ -8,20 +8,31 @@ Random random = new Random();
 var template = new Faker<Staff>()
     .StrictMode(true)
     .RuleFor(o => o.Id, f => Guid.NewGuid().ToString())
-    .RuleFor(o => o.No, f =>
-    {
-        no++;
-        return $"S{no.ToString().PadLeft(6, '0')}";
-    })
+    .RuleFor(
+        o => o.No,
+        f =>
+        {
+            no++;
+            return $"S{no.ToString().PadLeft(6, '0')}";
+        }
+    )
     .RuleFor(o => o.Name, f => f.Name.FirstName() + " " + f.Name.LastName())
     .RuleFor(o => o.MobileNo, f => f.Phone.PhoneNumber())
-    .RuleFor(o => o.DateOfBirth, f => f.Date.Between(DateTime.Now.AddYears(-30), DateTime.Now.AddYears(-20)))
-    .RuleFor(o => o.Salary, f =>
-    {
-        int randomNumber = random.Next(1000000, 1000000000);
-        var result = randomNumber.ToString().Substring(0, 1) + "0".PadLeft(randomNumber.ToString().Length - 1, '0');
-        return Convert.ToDecimal(result);
-    })
+    .RuleFor(
+        o => o.DateOfBirth,
+        f => f.Date.Between(DateTime.Now.AddYears(-30), DateTime.Now.AddYears(-20))
+    )
+    .RuleFor(
+        o => o.Salary,
+        f =>
+        {
+            int randomNumber = random.Next(1000000, 1000000000);
+            var result =
+                randomNumber.ToString().Substring(0, 1)
+                + "0".PadLeft(randomNumber.ToString().Length - 1, '0');
+            return Convert.ToDecimal(result);
+        }
+    )
     .RuleFor(o => o.Address, f => f.Address.FullAddress());
 
 var staff = template.Generate(100);
